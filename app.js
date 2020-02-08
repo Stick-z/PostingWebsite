faf8b5b24b63af924f7749b6c0cd93ed33746efe
Stick-z <56492228+Stick-z@users.noreply.github.com>
2020-02-08 09:14:52 -0800

    Update app.js
    Using localStorage

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-const list = document.getElementById("list")
-const enter = document.getElementById("enter")
+const list = document.getElementById("list");
+const enter = document.getElementById("enter");
 
 enter.addEventListener("click", add => {
-    var input = document.getElementById("form").value
-    var node = document.createElement("LI")
-    var text = document.createTextNode(input)
-    node.appendChild(text)
-    list.appendChild(node)
+    var input = document.getElementById("form").value;
+    var node = document.createElement("LI");
+    var text = document.createTextNode(input);
+    node.appendChild(text);
+    list.appendChild(node);
+    localStorage.setItem('items', document.getElementById("list").innerHTML);
 })
 document.querySelector('#list').addEventListener('click', function({target}) {
-  if (target.matches('li'))
-    target.remove()
+  if (target.matches('li'));
+    target.remove();
+    localStorage.setItem('items', document.getElementById("list").innerHTML);
 }, false);
+
+list.innerHTML = localStorage.getItem("items");
