6971584c205169c88e9436ae845451fc56a93d98
Stick-z <56492228+Stick-z@users.noreply.github.com>
2020-02-07 23:39:17 -0800

    Update app.js

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const list = document.getElementById("list")
 const enter = document.getElementById("enter")
 
 enter.addEventListener("click", add => {
-    localStorage("input", "document.getElementById("form").value")
-    localStorage("node", "document.createElement("LI")")
-    localStorage("node", "document.createTextNode(localStorage.getItem("input"))")
-    node.appendChild(localStorage.getItem("text"))
-    list.appendChild(localStorage.getItem("node"))
+    window.localStorage("input", "document.getElementById("form").value")
+    window.localStorage("node", "document.createElement("LI")")
+    window.localStorage("node", "document.createTextNode(window.localStorage.getItem("input"))")
+    node.appendChild(window.localStorage.getItem("text"))
+    list.appendChild(window.localStorage.getItem("node"))
 })
 document.querySelector('#list').addEventListener('click', function({target}) {
   if (target.matches('li'))
